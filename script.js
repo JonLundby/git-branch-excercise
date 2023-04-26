1f3974e72adc53f3061947d1fa49090e6c38f9b2
Jon <lundbyjon@hotmail.com>
2023-04-26 20:49:26 +0200

    fouth function added

--- script.js
+++ script.js
@@ -9,4 +9,9 @@ function startApp() {
 
 function anotherFunction() {
     console.log("just another function...")
+    fourthFunction();
+}
+
+function fourthFunction() {
+    console.log("the fourth is strong with this one!")
 }
