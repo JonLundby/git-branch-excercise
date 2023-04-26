0b53ba28ef597b6fcb117f4a58ac80614645f0f9
Jon <lundbyjon@hotmail.com>
2023-04-26 20:57:54 +0200

    ...

--- script.js
+++ script.js
@@ -9,9 +9,4 @@ function startApp() {
 
 function anotherFunction() {
     console.log("just another function...")
-    fourthFunction();
-}
-
-function fourthFunction() {
-    console.log("the fourth is strong with this one!")
 }
